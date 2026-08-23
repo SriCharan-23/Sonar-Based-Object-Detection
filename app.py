72ffb69bef1d2a3b0113f6f91988ff45fd1f5373
HIBARE SRI CHARAN RAO <sricharanrao451@gmail.com>
2026-08-23 12:46:55 +0530

    Configure app to use environment port and disable debug
    Update app to run on specified port and disable debug mode.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -58,4 +59,5 @@ def predict():
                                accuracy=accuracy)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
